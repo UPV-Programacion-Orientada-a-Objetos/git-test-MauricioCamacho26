c6bf6547aa636dab85f255ab4d034f8f2597b46b
MauricioCamacho26 <2230154@upv.edu.mx>
2024-01-11 21:43:26 -0600

    Modificacion de archvio README y Agregamos nuevo archivo

---
+++ ArchivoNuevo.java
@@ -0,0 +1,7 @@
+public class ArchivoNuevo {
+    public static void main(String[] args) {
+        System.out.println("Hello world belico!!");
+        System.out.println("Nuevo mundo 2");
+        
+    }
+}
